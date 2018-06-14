b2734bbea4f44e8acd8172d256a6239ce7ef90ac
kmills <ksmills82@gmail.com>
2018-06-13 23:24:37 -0400

    scrape on-click working

--- public/app.js
+++ public/app.js
@@ -1,6 +1,15 @@
 
-
-
+$(document).ready(function(){
+    $("#scrapeMe").on("click", function(){
+      $.ajax({
+        method: "GET",
+        url: "/scrape"
+      })
+        .done(function(data){
+          location.reload();
+          alert("You scraped " + data.length + " articles from foodnetwork.com");
+        });
+    });
 
 
 // Grab the articles as a json
@@ -10,7 +19,7 @@ $.getJSON("/articles", function(data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
-      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br /><a href='" + data[i].link + "'>See the full article here</a></p>");
+      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br /><a href='" + data[i].link + " target='_blank''>See the full article here</a></p>");
     }
   });
   
@@ -151,4 +160,4 @@ $.getJSON("/articles", function(data) {
     $("#titleinput").val("");
     $("#bodyinput").val("");
   });
-  
+});
